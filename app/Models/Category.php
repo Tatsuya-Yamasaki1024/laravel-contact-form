812398ede7d5c932190cb55ccd45612fc0853353
Tatsuya Yamasaki <zakkikki1024@yahoo.co.jp>
2026-06-28 18:28:24 +0900

    feat: モデル作成とリレーション定義 #4

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'content',
+    ];
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
+
 }
