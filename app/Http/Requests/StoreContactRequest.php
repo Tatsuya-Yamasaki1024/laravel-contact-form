26120144bad6dfca717ede6e195c07fe42b513c4
Tatsuya Yamasaki <zakkikki1024@yahoo.co.jp>
2026-07-01 19:11:29 +0900

    feat: コンタクトCRUD実装 #7

--- app/Http/Requests/StoreContactRequest.php
+++ app/Http/Requests/StoreContactRequest.php
@@ -34,4 +34,20 @@ class StoreContactRequest extends FormRequest
             'detail' => ['required', 'string', 'max:120'],
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'last_name.required' => '名を入力してください',
+            'first_name.required' => '姓を入力してください',
+            'gender.required' => '性別を選択してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'tel.required' => '電話番号を入力してください',
+            'address.required' => '住所を入力してください',
+            'category_id.required' => 'お問い合わせの種類を選択してください',
+            'detail.required' => 'お問い合わせ内容を入力してください',
+            'detail.max' => 'お問い合わせ内容は120文字以内で入力してください',
+        ];
+    }
 }
